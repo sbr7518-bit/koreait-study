575e4fff4d16e6d28353ea3a4b8ccc25c0805833
sbr7518 <sbr7518@gmail.com>
2026-02-09 21:26:59 +0900

    Update Game.java
    집에서 수정한 내용

--- 3.java/javaStudy/src/kr/co/training/Game.java
+++ 3.java/javaStudy/src/kr/co/training/Game.java
@@ -3,18 +3,19 @@ package kr.co.training;
 import java.util.Scanner;
 
 public class Game {
+	static Scanner sc = new Scanner(System.in);
 	
 	public static void main(String[] args) {
 		startGame(); // 게임 시작
 	}
+	
 	public static void startGame() {
 		// Scanner 객체 생성
-		Scanner sc = new Scanner(System.in);
 		int menu;
-		// Warlod 객체 생성
-		Warlord hero = new Warlord();  
 		// CharacterAbility 객체 생성
-		CharaterAbility abilty = new CharaterAbility();
+		CharaterAbility ch = new CharaterAbility();
+		Warlord hero = new Warlord(ch);
+		
 		
 		do { // 게임 시작
 			System.out.println("게임 메뉴를 선택해주세요.");
@@ -28,13 +29,13 @@ public class Game {
 			menu = sc.nextInt();	
 			System.out.println("================");
 			if(menu == 1) {
-				huntMenu(); // 사냥 시작
+				huntMenu(hero, ch); // 사냥 시작
 			} else if(menu == 2) {
-				setPorsion(); // 인벤토리-포션
+				setPorsion(ch); // 인벤토리-포션
 			} else if(menu == 3) {
-				setStatus(); // 스텟
+				setStatus(ch); // 스텟
 			} else if(menu == 4) {
-				getCharInfo(); // 케릭터 정보 확인
+				getCharInfo(ch, hero); // 케릭터 정보 확인
 			} else if(menu == 0) {
 				System.out.println("게임을 종료합니다."); // 종료
 				break;
@@ -45,14 +46,9 @@ public class Game {
 		} while(true);
 	}
 	
-	public static boolean huntMenu(Warlord hero) {
+	public static boolean huntMenu(Warlord hero, CharaterAbility ch) {
 		// MonsterAbility 객체 생성
 		MonsterAbility mons = new MonsterAbility();
-		CharaterAbility ch = new CharaterAbility();
-		
-		Scanner sc = new Scanner(System.in);
-		int menu;
-		int selectAttack;
 		
 		do {
 			System.out.println("공격을 선택해주세요.");
@@ -63,8 +59,7 @@ public class Game {
 			System.out.println("0. 처음으로 돌아가기");
 			System.out.print("메뉴 선택 : ");
 			// 스캐너로 입력 받기
-			menu = sc.nextInt();
-			selectAttack = menu;
+			int selectAttack = sc.nextInt();
 			System.out.println("================");
 			
 			if(selectAttack == 1) {
@@ -88,17 +83,16 @@ public class Game {
 				System.out.println("================");
 				System.out.println("올바른 메뉴를 선택해주세요.");
 			}
+			if(mons.getHp() <= 0) {
+				System.out.println("사냥이 종료되었습니다.");
+				return true;
+			}
 			System.out.println("================");
-
-		} while(true);
-	}
+		} while(true); 
+	 }
 	
 
 	public static boolean setPorsion(CharaterAbility ch) {
-		Scanner sc = new Scanner(System.in);
-		int menu;
-		int selectPorsion;
-		setPorsion(ch);
 		do {
 			System.out.println("인벤토리-포션");
 			System.out.println("1. 체력 포션");
@@ -107,8 +101,7 @@ public class Game {
 			System.out.println("0. 처음으로 돌아가기");
 			System.out.print("사용할 포션을 선택하세요: ");
 			// 스캐너로 입력 받기
-			menu = sc.nextInt();
-			selectPorsion = menu;
+			int selectPorsion = sc.nextInt();
 			System.out.println("================");
 			
 			if(selectPorsion == 1) {
@@ -121,9 +114,11 @@ public class Game {
 //	             => "인벤토리 체력 포션이 없습니다." 출력 
 				if(ch.gethpPortionCount() >= 1) {
 					System.out.println("체력 포션을 사용했습니다.");
-					ch.addHp(-1);
+					ch.sethpPortionCount(ch.gethpPortionCount()-1);
+					ch.addHp(60);
+					System.out.println("포션 사용 후 현재 체력 : " + ch.getHp());
 				} else if(ch.gethpPortionCount() == 0){
-					
+					System.out.println("인벤토리에 체력 포션이 없습니다.");
 				}
 				
 			} else if(selectPorsion == 2) {
@@ -134,9 +129,19 @@ public class Game {
 //	             => "포션 사용 후 현재 마력 : OO"
 //	         > 소지중 마력 포션이 0개 일 경우
 //	             => "인벤토리 마력 포션이 없습니다." 출력 
+				if(ch.getmpPortionCount() >= 1) {
+					System.out.println("마력 포션을 사용했습니다.");
+					ch.setmpPortionCount(ch.getmpPortionCount()-1);
+					ch.addMp(60);
+					System.out.println("포션 사용 후 현재 마력 : " + ch.getMp());
+				} else if(ch.getmpPortionCount() == 0){
+					System.out.println("인벤토리에 마력 포션이 없습니다.");
+				}
 			} else if(selectPorsion == 3) {
 //		         > "소지중인 체력 포션은 OO개 입니다." 출력
+				System.out.println("소지중인 체력 포션은 " + ch.gethpPortionCount() + "개 입니다.");
 //		         > "소지중인 마력 포션은 OO개 입니다." 출력 
+				System.out.println("소지중인 마력 포션은 " + ch.getmpPortionCount() + "개 입니다.");
 			} else if(selectPorsion == 0) {
 				// 처음으로 돌아가기
 				System.out.println("처음 메뉴로 돌아갑니다.");
@@ -149,7 +154,7 @@ public class Game {
 		} while(true);
 	}
 	
-	public static boolean setStatus() {
+	public static boolean setStatus(CharaterAbility ch) {
 		// 레벨업 할때마다 스텟(unusedStatus) 5가 생김
 		// 현재 레벨 확인 후 여유분의 스텟을 추가하는 코드 작성
 		// * ability의 setter/getter를 이용
@@ -171,7 +176,14 @@ public class Game {
 //                => "증가된 후 힘 : OO" 출력 
 //            > 사용 가능한 잔여 스텟(unsedStatus)이 없을 경우
 //                => "사용 가능한 스텟이 없습니다." 출력
-				}
+				 if (ch.getunusedStatus() >= 5) {
+		                ch.setStr(ch.getStr() + 5);
+		                ch.setunusedStatus(ch.getunusedStatus() - 5);
+		                System.out.println("힘이 5 증가되었습니다.");
+		                System.out.println("증가된 후 힘 : " + ch.getStr());
+		            } else {
+		                System.out.println("사용 가능한 스텟이 없습니다.");
+		            }
 			} else if(selectStatus == 2) {
 //	          > 사용 가능한 잔여 스텟(unusedStatus)이 있을 경우
 //                => "민첩이 5 증가되었습니다." 출력
@@ -180,6 +192,14 @@ public class Game {
 //                => "증가된 후 민첩 : OO" 출력 
 //            > 사용 가능한 잔여 스텟(unsedStatus)이 없을 경우
 //                => "사용 가능한 스텟이 없습니다." 출력
+				if (ch.getunusedStatus() >= 5) {
+	                ch.setDex(ch.getDex() + 5);
+	                ch.setunusedStatus(ch.getunusedStatus() - 5);
+	                System.out.println("민첩이 5 증가되었습니다.");
+	                System.out.println("증가된 후 민첩 : " + ch.getDex());
+	            } else {
+	                System.out.println("사용 가능한 스텟이 없습니다.");
+	            }
 			} else if(selectStatus == 3) {
 //	          > 사용 가능한 잔여 스텟(unusedStatus)이 있을 경우
 //                => "지력이 5 증가되었습니다." 출력
@@ -188,7 +208,14 @@ public class Game {
 //                => "증가된 후 지력 : OO" 출력
 //            > 사용 가능한 잔여 스텟(unsedStatus)이 없을 경우
 //                => "사용 가능한 스텟이 없습니다." 출력
-			
+				if (ch.getunusedStatus() >= 5) {
+	                ch.setKnowledge(ch.getKnowledge() + 5);
+	                ch.setunusedStatus(ch.getunusedStatus() - 5);
+	                System.out.println("지력이 5 증가되었습니다.");
+	                System.out.println("증가된 후 지력 : " + ch.getKnowledge());
+	            } else {
+	                System.out.println("사용 가능한 스텟이 없습니다.");
+	            }
 			} else if(selectStatus == 0) {
 				// 처음으로 돌아가기
 				System.out.println("처음 메뉴로 돌아갑니다.");
@@ -202,7 +229,7 @@ public class Game {
 		} while(true);
 	}
 	
-	public static boolean getCharInfo() {
+	public static boolean getCharInfo(CharaterAbility ch, Warlord hero) {
 //        - 현재 가진 정보들을 전체 출력
 //			> "========케릭터 정보========"
 //        	> "체력 : OO"
@@ -213,6 +240,17 @@ public class Game {
 //        	> "현재 레벨 : OO"
 //        	> "현재 경험치 : OO"
 //        	> "사용 가능 스텟 : OO" 
-		System.out.println("================");
+		   System.out.println("======== 캐릭터 정보 ========");
+		    System.out.println("체력 : " + ch.getHp());
+		    System.out.println("마나 : " + ch.getMp());
+		    System.out.println("힘 : " + hero.getStr());
+		    System.out.println("민첩 : " + hero.getDex());
+		    System.out.println("지력 : " + hero.getKnowledge());
+		    System.out.println("현재 레벨 : " + ch.getLevel());
+		    System.out.println("현재 경험치 : " + ch.getExp());
+		    System.out.println("사용 가능 스텟 : " + ch.getunusedStatus());
+		    System.out.println("============================");
 		return true;
 	}
+
+}
